28413e44acf75b605d15cee40191f56282af4dcd
Somnath Choudhury <102171721+somnath-choudhury@users.noreply.github.com>
2025-08-19 19:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 function createTable() {
-    //Write your code here
-  
+  const rn = prompt("Input number of rows");
+  const cn = prompt("Input number of columns");
+
+  if (rn < 0 || cn < 0) {
+    alert('Please enter positive values')
+  }
+
+  if (isNaN(rn) || isNaN(rn)) {
+    return;
+  }
+
+  const table = document.getElementById("myTable");
+
+  table.innerHTML = ''
+
+  for (let i = 0; i < rn; i++) {
+    const row = table.insertRow(i);
+
+    for (let j = 0; j < cn; j++) {
+      const cell = row.insertCell(j);
+
+      cell.textContent = `Row-${i} Column-${j}`
+    }
+  }
 }
